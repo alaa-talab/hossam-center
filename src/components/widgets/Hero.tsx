993c7ae1883b7cd7770173696081ac4cb43ba401
Alaa Talab <alaatalab99@gmail.com>
2024-09-25 21:45:09 +0300

    Update Hero.tsx

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -5,38 +5,42 @@ import CTA from '../common/CTA';
 const Hero = ({ title, subtitle, tagline, callToAction, callToAction2, image }: HeroProps) => {
   return (
     <section id="heroOne">
-    <div className="relative w-full h-[80vh] sm:h-[90vh] lg:h-[100vh] overflow-hidden object-fill">
-      {image && (
-        <Image
-          className="absolute right-2 w-full h-full object-cover object-center "
-          src={image.src}
-          alt={image.alt}
-          layout="fill"
-          sizes="100vw"
-          priority
-        />
-      )}
-    </div>
-    <div className="relative z-10 mx-auto max-w-full text-center py-12 md:py-20 bg-gradient-to-b from-black/80 to-black/0 text-white ">
-      {tagline && (
-        <p className="text-base font-semibold uppercase tracking-wide text-primary-600 dark:text-primary-200">
-          {tagline}
-        </p>
-      )}
-      {title && (
-        <h1 className="leading-tighter font-heading mb-6 text-2xl font-bold tracking-tighter md:text-5xl lg:text-5xl ">
-          {title}
-        </h1>
-      )}
-      <div className="mx-auto max-w-3xl">
-        {subtitle && <p className="mb-6 text-xl font-normal text-gray-100 dark:text-slate-200">{subtitle}</p>}
-        <div className="flex max-w-none flex-col flex-nowrap gap-4 px-4 sm:flex-row sm:justify-center">
-          {callToAction && <CTA callToAction={callToAction} linkClass="btn btn-primary" />}
-          {callToAction2 && <CTA callToAction={callToAction2} linkClass="btn" />}
+      <div className="mx-auto max-w-7xl px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+          <div className="mx-auto max-w-4xl pb-10 text-center md:pb-16">
+          {image && (
+            <div className="relative m-auto max-w-5xl">
+              <Image
+                className="mx-auto h-auto w-full rounded-md bg-gray-400 dark:bg-slate-700"
+                src={image.src}
+                alt={image.alt}
+                width={1024}
+                height={607}
+                sizes="(max-width: 64rem) 100vw, 1024px"
+                loading="eager"
+                placeholder="blur"
+                priority
+              />
+            </div>
+          )}
+          <br></br>
+            {title && (
+              <h1 className="leading-tighter font-heading mb-6 text-4xl font-bold tracking-tighter md:text-5xl lg:text-4xl">
+                {title}
+              </h1>
+            )}
+            <div className="mx-auto max-w-3xl">
+              {subtitle && <p className="mb-6 text-xl font-normal text-gray-600 dark:text-slate-400">{subtitle}</p>}
+              <div className="flex max-w-none flex-col flex-nowrap gap-4 px-4 sm:flex-row sm:justify-center">
+                {callToAction && <CTA callToAction={callToAction} linkClass="btn btn-primary" />}
+                {callToAction2 && <CTA callToAction={callToAction2} linkClass="btn" />}
+              </div>
+            </div>
+          </div>
+         
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 };
 
