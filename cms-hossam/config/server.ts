a7befdb6fc84a05a316bade2f19a6b8fc2e0b286
Alaa Talab <72857849+alaa-talab@users.noreply.github.com>
2024-10-01 11:46:47 +0300

    update

--- cms-hossam/config/server.ts
+++ cms-hossam/config/server.ts
@@ -1,4 +1,10 @@
-export default ({ env }) => ({
+interface Env {
+  (variable: string, defaultValue?: string): string;
+  int(variable: string, defaultValue?: number): number;
+  array(variable: string): string[]; // Handle array type for APP_KEYS
+}
+
+export default ({ env }: { env: Env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
